1d6eb0dc0498fba654767f99b0b01cab56435783
unknown <joye@SHA-L7-JOYE.amd.com>
2015-04-11 23:01:14 +0800

    first submit

---
+++ ZFXD3D/dlgChangeDevice.h
@@ -0,0 +1,14 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define dlgChangDevice                          101
+#define IDOK                                    40000
+#define IDCANCEL                                40001
+#define IDC_WND                                 40002
+#define IDC_FULL                                40003
+#define IDC_DEVICE                              40004
+#define ID_BACKFMT                              40005
+#define IDC_MODE                                40006
+#define IDC_ADAPTERFMT                          40007
+#define IDC_ADAPTER                             40008
---
+++ ZFXD3D/resource.h
@@ -0,0 +1,13 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDC_OK                                  40000
+#define IDC_WND                                 40002
+#define IDC_FULL                                40003
+#define IDC_DEVICE                              40004
+#define IDC_CANCEL                              40005
+#define IDC_MODE                                40006
+#define IDC_ADAPTERFMT                          40007
+#define IDC_ADAPTER                             40008
+#define IDC_BACKFMT                             40009
